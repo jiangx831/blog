692e91253ba2ca7962224296ce37e81c818b1c50
xue.jiang <xue.jiang@faw-vw.com>
2022-06-16 09:46:30 +0800

    first commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: 'Mew\'s blog',
+  description: '我的博客',
+  base: '/blog/',
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '关于', link: '/about/' },
+    ], 
+    sidebar: []
+  }
+}
